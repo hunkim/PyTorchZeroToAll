ef4920e3cf355c48732b56320332c803126adfcc
Sung Kim <hunkim@gmail.com>
2017-10-30 00:16:14 +0800

    Added more lectures

--- 09_01_softmax_loss.py
+++ 09_01_softmax_loss.py
@@ -9,30 +9,28 @@ from torch.autograd import Variable
 # Cross entropy example
 import numpy as np
 # One hot
-# 0: 1 0 0 0
-# 1: 0 1 0 0
-# 2: 0 0 1 0
-# 3: 0 0 0 1
-Y = np.array([0, 0, 1, 0])
+# 0: 1 0 0
+# 1: 0 1 0
+# 2: 0 0 1
+Y = np.array([1, 0, 0])
 
-Y_pred1 = np.array([0.1, 0.09, 0.8, 0.01])
-Y_pred2 = np.array([0.8, 0.09, 0.1, 0.01])
+Y_pred1 = np.array([0.7, 0.2, 0.1])
+Y_pred2 = np.array([0.1, 0.3, 0.6])
 print("loss1 = ", np.sum(-Y * np.log(Y_pred1)))
 print("loss2 = ", np.sum(-Y * np.log(Y_pred2)))
-
 
 # Softmax + CrossEntropy (logSoftmax + NLLLoss)
 loss = nn.CrossEntropyLoss()
 
 # target is of size nBatch
-# each element in target has to have 0 <= value < nClasses (0-3)
-# Input is class, not onthot
-Y = Variable(torch.LongTensor([2]), requires_grad=False)
+# each element in target has to have 0 <= value < nClasses (0-2)
+# Input is class, not one-hot
+Y = Variable(torch.LongTensor([0]), requires_grad=False)
 
 # input is of size nBatch x nClasses = 1 x 4
 # Y_pred are logits (not softmax)
-Y_pred1 = Variable(torch.Tensor([[0.1, 0.09, 0.9, 0.1]]))
-Y_pred2 = Variable(torch.Tensor([[0.8, 0.2, 0.3, 0.1]]))
+Y_pred1 = Variable(torch.Tensor([[2.0, 1.0, 0.1]]))
+Y_pred2 = Variable(torch.Tensor([[0.5, 2.0, 0.3]]))
 
 l1 = loss(Y_pred1, Y)
 l2 = loss(Y_pred2, Y)
@@ -43,20 +41,20 @@ print("Y_pred1=", torch.max(Y_pred1.data, 1)[1])
 print("Y_pred2=", torch.max(Y_pred2.data, 1)[1])
 
 # target is of size nBatch
-# each element in target has to have 0 <= value < nClasses (0-3)
-# Input is class, not ont hot
-Y = Variable(torch.LongTensor([2, 3, 0]), requires_grad=False)
+# each element in target has to have 0 <= value < nClasses (0-2)
+# Input is class, not one-hot
+Y = Variable(torch.LongTensor([2, 0, 1]), requires_grad=False)
 
 # input is of size nBatch x nClasses = 2 x 4
 # Y_pred are logits (not softmax)
-Y_pred1 = Variable(torch.Tensor([[0.1, 0.09, 0.9, 0.1],
-                                 [0.1, 0.09, 0.2, 0.9],
-                                 [0.7, 0.09, 0.1, 0.1]]))
+Y_pred1 = Variable(torch.Tensor([[0.1, 0.2, 0.9],
+                                 [1.1, 0.1, 0.2],
+                                 [0.2, 2.1, 0.1]]))
 
 
-Y_pred2 = Variable(torch.Tensor([[0.8, 0.2, 0.3, 0.5],
-                                 [0.8, 0.2, 0.3, 0.5],
-                                 [0.2, 0.2, 0.3, 0.5]]))
+Y_pred2 = Variable(torch.Tensor([[0.8, 0.2, 0.3],
+                                 [0.2, 0.3, 0.5],
+                                 [0.2, 0.2, 0.5]]))
 
 l1 = loss(Y_pred1, Y)
 l2 = loss(Y_pred2, Y)
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # PyTorchZeroToAll
 Quick 3~4 day lecture materials for HKUST students.
 
-* [Video Lectures: Lecture 1 to Lecture 7. Others TBA)](http://bit.ly/PyTorchVideo)
+* [Video Lectures: CNN, RNN TBA)](http://bit.ly/PyTorchVideo)
 * [Lecture Slides @GoogleDrive](http://bit.ly/PyTorchZeroAll)
 
 If you cannot access the GoogleDoc for somehow, please check out pdf files in slides. However, slides in GoogleDrive are always latest. We really appreciate your comments.

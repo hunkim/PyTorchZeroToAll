c4610ff26a01a0622bc11dcac0f0812f05c56e0c
Sung Kim <hunkim@gmail.com>
2017-11-03 13:31:39 +0800

    Added fixed code

--- 12_4_name_classify.py
+++ 12_4_name_classify.py
@@ -11,7 +11,7 @@ from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 
 HIDDEN_SIZE = 100
 N_LAYERS = 1
-BATCH_SIZE = 32
+BATCH_SIZE = 256
 N_EPOCHS = 20
 
 test_dataset = NameDataset(is_test_set=True)
@@ -55,7 +55,8 @@ def pad_sequences(vectorized_seqs, seq_lengths, countries):
     seq_tensor = seq_tensor[perm_idx]
 
     target = countries2tensor(countries)
-    target = target[perm_idx]
+    if len(countries):
+        target = target[perm_idx]
 
     return cuda_variable(seq_tensor), \
         cuda_variable(seq_lengths), \
@@ -96,28 +97,25 @@ class RNNClassifier(nn.Module):
 
     def forward(self, input, seq_lengths):
         # Note: we run this all at once (over the whole input sequence)
-        # input shape: S x B (input size)
-        batch_size = len(input[1])
+        # input shape: B x S (input size)
+        # transpose to make S(sequence) x B (batch)
+        input = input.t()
+        batch_size = input.size(1)
 
-        # Get hidden
+        # Make a hidden
         hidden = self._init_hidden(batch_size)
 
-        # input shape: S x B (input size)
-        embeded = self.embedding(input)  # S x B -> S x B x I (embedding size)
+        # Embedding S x B -> S x B x I (embedding size)
+        embedded = self.embedding(input)
 
-        gru_input = embeded
-        # FIXME: is this a right way? It makes training and testing slower
-        # With pack: [1m 0s (20 95%) 0.0596]
-        # Without pack: [0m 45s (20 95%) 0.0901]
-        # pack them up nicely
-        # gru_input = pack_padded_sequence(embeded, seq_lengths.cpu().numpy())
+        # Pack them up nicely
+        gru_input = pack_padded_sequence(embedded, seq_lengths.data.cpu().numpy())
 
         # To compact weights again call flatten_parameters().
         self.gru.flatten_parameters()
-
         output, hidden = self.gru(gru_input, hidden)
-        fc_output = self.fc(hidden[-1])  # Use the last layer
-        print("Final output size", fc_output.size())
+        # Use the last layer output
+        fc_output = self.fc(hidden[-1])
         return fc_output
 
     def _init_hidden(self, batch_size):
@@ -129,41 +127,53 @@ class RNNClassifier(nn.Module):
 # It feeds single string to demonstrate seq2seq
 # It's extremely slow, and we need to use (1) batch and (2) data parallelism
 # http://pytorch.org/tutorials/beginner/former_torchies/parallelism_tutorial.html.
-def train(names, countries):
-    input, seq_lengths, target = make_variables(names, countries)
+def train():
+    total_loss = 0
+    train_dataset_size = len(train_loader.dataset)
 
-    # transpose to make S(sequence) x B (batch)
-    output = classifier(input.t(), seq_lengths)
-
-    # FIXME: output size is Batch*n_GPUs * Inputsize
-    print("output size", output.size())
-    loss = criterion(output, target)
-
-    classifier.zero_grad()
-    loss.backward()
-    optimizer.step()
-
-    return loss.data[0]
-
-
-def test():
-    print("evaluating ...")
-    correct = 0
-
-    for i, (names, countries) in enumerate(test_loader):
+    for i, (names, countries) in enumerate(train_loader, 1):
         input, seq_lengths, target = make_variables(names, countries)
+        output = classifier(input, seq_lengths)
 
-        # transpose to make S(sequence) x B (batch)
-        output = classifier(input.t(), seq_lengths)
-        print(i, output.size())
-        print(target.size())
+        loss = criterion(output, target)
+        total_loss += loss.data[0]
 
+        classifier.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if i % 100 is 1:
+            trained_size = i * BATCH_SIZE
+            print('[%s (%d %d%%) %.4f]' %
+                  (time_since(start), epoch, trained_size * 100 / train_dataset_size,
+                   total_loss / trained_size))
+
+    return total_loss
+
+
+# Testing cycle
+def test(name=None):
+    # Predict for a given name
+    if name:
+        input, seq_lengths, target = make_variables([name], [])
+        output = classifier(input, seq_lengths)
+        pred = output.data.max(1, keepdim=True)[1]
+        country_id = pred.cpu().numpy()[0][0]
+        print(name, "is", train_dataset.get_country(country_id))
+        return
+
+    print("evaluating trained model ...")
+    correct = 0
+    train_data_size = len(test_loader.dataset)
+
+    for names, countries in test_loader:
+        input, seq_lengths, target = make_variables(names, countries)
+        output = classifier(input, seq_lengths)
         pred = output.data.max(1, keepdim=True)[1]
         correct += pred.eq(target.data.view_as(pred)).cpu().sum()
 
     print('\nTest set: Accuracy: {}/{} ({:.0f}%)\n'.format(
-        correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
+        correct, train_data_size, 100. * correct / train_data_size))
 
 
 if __name__ == '__main__':
@@ -171,6 +181,7 @@ if __name__ == '__main__':
     classifier = RNNClassifier(N_CHARS, HIDDEN_SIZE, n_countries, N_LAYERS)
     if torch.cuda.device_count() > 1:
         print("Let's use", torch.cuda.device_count(), "GPUs!")
+        # Default dim = 0 [33, xxx] -> [11, ...], [11, ...], [11, ...] on 3 GPUs
         classifier = nn.DataParallel(classifier)
 
     if torch.cuda.is_available():
@@ -182,14 +193,13 @@ if __name__ == '__main__':
     start = time.time()
     print("Training for %d epochs..." % N_EPOCHS)
     for epoch in range(1, N_EPOCHS + 1):
-        loss = 0
-
-        for i, (names, countries) in enumerate(train_loader):
-            loss += train(names, countries)  # Batch size is 1
-
-            if i % 100 == 0:
-                print('[%s (%d %d%%) %.4f]' %
-                      (time_since(start), epoch, i * BATCH_SIZE * 100 / len(train_loader.dataset), loss / (i + 1)))
+        # Train cycle
+        train()
 
         # Testing
         test()
+        test("Sung")
+        test("Jungwoo")
+        test("Nako")
+
+

0e7399db131c734efb47cd87b879cfaa4701e163
Mahir Atmis <atmismahir@gmail.com>
2018-03-14 13:28:09 +0300

    Stochastic Gradient Descent

--- 02_manual_gradient.py
+++ 02_manual_gradient.py
@@ -1,8 +1,11 @@
+import matplotlib.pyplot as plt
+
 x_data = [1.0, 2.0, 3.0]
 y_data = [2.0, 4.0, 6.0]
 
-w = 1.0  # a random guess: random value
-
+w = 1.0    # a random guess of initial weight
+lr = 0.01  # learning rate
+num_epoches = 10
 # our model forward pass
 
 
@@ -13,25 +16,53 @@ def forward(x):
 # Loss function
 def loss(x, y):
     y_pred = forward(x)
-    return (y_pred - y) * (y_pred - y)
+    return (y_pred - y) ** 2
 
 
 # compute gradient
-def gradient(x, y):  # d_loss/d_w
+# d_loss/d_w
+# loss = (wx-y)^2
+def gradient(x, y):
     return 2 * x * (x * w - y)
 
-# Before training
-print("predict (before training)",  4, forward(4))
 
+# Before training
+print("predict (before training)", 4, forward(4))
+
+# plotting purposes
+w_history = []
+loss_history = []
 # Training loop
-for epoch in range(10):
+for epoch in range(num_epoches):
     for x_val, y_val in zip(x_data, y_data):
         grad = gradient(x_val, y_val)
-        w = w - 0.01 * grad
+        w = w - lr * grad
         print("\tgrad: ", x_val, y_val, round(grad, 2))
-        l = loss(x_val, y_val)
+        los_val = loss(x_val, y_val)
+        w_history.append(w)
+        loss_history.append(los_val)
 
-    print("progress:", epoch, "w=", round(w, 2), "loss=", round(l, 2))
+    print("progress:", epoch, "w=", round(w, 2), "loss=", round(los_val, 2))
 
 # After training
-print("predict (after training)",  "4 hours", forward(4))
+print("predict (after training)", "4 hours", forward(4))
+
+# Plot Graphs
+colors = ('b', 'g', 'r', 'c', 'm', 'y', 'k')
+style = ('s', '8', 'X', 'p', 'd', 'D', '*')
+
+# Plot weight vs Loss
+plt.subplot(2, 1, 1)
+plt.plot(w_history, loss_history, marker=style[1], color=colors[1],
+         markersize=5)
+plt.ylabel('Loss')
+plt.xlabel('w')
+
+# Plot iterations vs weight
+plt.subplot(2, 1, 2)
+plt.plot(range(1, len(w_history) + 1), w_history, marker=style[1],
+         color=colors[1], markersize=5)
+plt.ylabel('w')
+plt.xlabel('Iteration')
+
+plt.show()

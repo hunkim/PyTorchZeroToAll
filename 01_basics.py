9de5f238bea7420535109c23b56252ce5ea2f994
Aaditya Paul <3x3paul@gmail.com>
2018-06-05 18:01:55 +0530

    Update 01_basics.py

--- 01_basics.py
+++ 01_basics.py
@@ -23,10 +23,10 @@ for w in np.arange(0.0, 4.1, 0.1):
     print("w=", w)
     l_sum = 0
     for x_val, y_val in zip(x_data, y_data):
-        y_pred_val = forward(x_val)
+#         y_pred_val = forward(x_val)
         l = loss(x_val, y_val)
         l_sum += l
-        print("\t", x_val, y_val, y_pred_val, l)
+        print("\t", x_val, y_val,l)
     print("MSE=", l_sum / 3)
     w_list.append(w)
     mse_list.append(l_sum / 3)

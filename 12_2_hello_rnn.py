4328980cfe1bd12fe2ae3d825cd35c35d99bc76c
Mehrdad Yazdani <myazdani@gmail.com>
2018-07-31 09:30:24 -0700

    reshape target label
    label dimension mismatch causes error in 0.4.1

--- 12_2_hello_rnn.py
+++ 12_2_hello_rnn.py
@@ -75,7 +75,7 @@ for epoch in range(100):
         hidden, output = model(hidden, input)
         val, idx = output.max(1)
         sys.stdout.write(idx2char[idx.data[0]])
-        loss += criterion(output, label)
+        loss += criterion(output, label.view(1))
 
     print(", epoch: %d, loss: %1.3f" % (epoch + 1, loss.data[0]))
 

2d40855d83031b1bdbb81e32bb8b8eece44c259a
Hammania689 <hameedabdulrashid@gmail.com>
2019-06-18 19:19:51 -0400

    Adding comments and updates to pytorch :)

--- 03_auto_gradient.py
+++ 03_auto_gradient.py
@@ -1,39 +1,34 @@
 import torch
-from torch.autograd import Variable
+import pdb
 
 x_data = [1.0, 2.0, 3.0]
 y_data = [2.0, 4.0, 6.0]
-
-w = Variable(torch.Tensor([1.0]),  requires_grad=True)  # Any random value
+w = torch.tensor([1.0], requires_grad=True)
 
 # our model forward pass
-
-
 def forward(x):
     return x * w
 
 # Loss function
-
-
-def loss(x, y):
-    y_pred = forward(x)
-    return (y_pred - y) * (y_pred - y)
+def loss(y_pred, y_val):
+    return (y_pred - y_val) ** 2
 
 # Before training
-print("predict (before training)",  4, forward(4).data[0])
+print("Prediction (before training)",  4, forward(4).item())
 
 # Training loop
 for epoch in range(10):
     for x_val, y_val in zip(x_data, y_data):
-        l = loss(x_val, y_val)
-        l.backward()
-        print("\tgrad: ", x_val, y_val, w.grad.data[0])
-        w.data = w.data - 0.01 * w.grad.data
+        y_pred = forward(x_val) # 1) Forward pass
+        l = loss(y_pred, y_val) # 2) Compute loss
+        l.backward() # 3) Back propagation to update weights
+        print("\tgrad: ", x_val, y_val, w.grad.item())
+        w.data = w.data - 0.01 * w.grad.item()
 
         # Manually zero the gradients after updating weights
         w.grad.data.zero_()
 
-    print("progress:", epoch, l.data[0])
+    print(f"Epoch: {epoch} | Loss: {l.item()}")
 
 # After training
-print("predict (after training)",  4, forward(4).data[0])
+print("Prediction (after training)",  4, forward(4).item())

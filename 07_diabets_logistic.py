7f6808fbba7915150d669e22ca45590234668819
Hammania689 <hameedabdulrashid@gmail.com>
2019-06-21 21:01:57 -0400

    Refactoring lesson #7. Will not provide notes for this lesson

--- 07_diabets_logistic.py
+++ 07_diabets_logistic.py
@@ -1,28 +1,23 @@
-
-import torch
-from torch.autograd import Variable
+from torch import nn, optim, from_numpy
 import numpy as np
 
 xy = np.loadtxt('./data/diabetes.csv.gz', delimiter=',', dtype=np.float32)
-x_data = Variable(torch.from_numpy(xy[:, 0:-1]))
-y_data = Variable(torch.from_numpy(xy[:, [-1]]))
-
-print(x_data.data.shape)
-print(y_data.data.shape)
+x_data = from_numpy(xy[:, 0:-1])
+y_data = from_numpy(xy[:, [-1]])
+print(f'X\'s shape: {x_data.shape} | Y\'s shape: {y_data.shape}')
 
 
-class Model(torch.nn.Module):
-
+class Model(nn.Module):
     def __init__(self):
         """
         In the constructor we instantiate two nn.Linear module
         """
         super(Model, self).__init__()
-        self.l1 = torch.nn.Linear(8, 6)
-        self.l2 = torch.nn.Linear(6, 4)
-        self.l3 = torch.nn.Linear(4, 1)
+        self.l1 = nn.Linear(8, 6)
+        self.l2 = nn.Linear(6, 4)
+        self.l3 = nn.Linear(4, 1)
 
-        self.sigmoid = torch.nn.Sigmoid()
+        self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
         """
@@ -35,6 +30,7 @@ class Model(torch.nn.Module):
         y_pred = self.sigmoid(self.l3(out2))
         return y_pred
 
+
 # our model
 model = Model()
 
@@ -42,17 +38,17 @@ model = Model()
 # Construct our loss function and an Optimizer. The call to model.parameters()
 # in the SGD constructor will contain the learnable parameters of the two
 # nn.Linear modules which are members of the model.
-criterion = torch.nn.BCELoss(size_average=True)
-optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
+criterion = nn.BCELoss(reduction='mean')
+optimizer = optim.SGD(model.parameters(), lr=0.1)
 
 # Training loop
 for epoch in range(100):
-        # Forward pass: Compute predicted y by passing x to the model
+    # Forward pass: Compute predicted y by passing x to the model
     y_pred = model(x_data)
 
     # Compute and print loss
     loss = criterion(y_pred, y_data)
-    print(epoch, loss.data[0])
+    print(f'Epoch: {epoch + 1}/100 | Loss: {loss.item():.4f}')
 
     # Zero gradients, perform a backward pass, and update the weights.
     optimizer.zero_grad()

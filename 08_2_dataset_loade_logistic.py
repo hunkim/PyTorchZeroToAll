933aca18a7e4ebaa88ee2f606e3be97374c6f2b4
Hammania689 <hameedabdulrashid@gmail.com>
2019-06-21 21:14:18 -0400

    Refactored code for tutorial on Pytorch dataloader

--- 08_2_dataset_loade_logistic.py
+++ 08_2_dataset_loade_logistic.py
@@ -1,22 +1,20 @@
 # References
 # https://github.com/yunjey/pytorch-tutorial/blob/master/tutorials/01-basics/pytorch_basics/main.py
 # http://pytorch.org/tutorials/beginner/data_loading_tutorial.html#dataset-class
-import torch
-import numpy as np
-from torch.autograd import Variable
 from torch.utils.data import Dataset, DataLoader
+from torch import nn, from_numpy, optim
+import numpy as np
 
 
 class DiabetesDataset(Dataset):
     """ Diabetes dataset."""
-
     # Initialize your data, download, etc.
     def __init__(self):
         xy = np.loadtxt('./data/diabetes.csv.gz',
                         delimiter=',', dtype=np.float32)
         self.len = xy.shape[0]
-        self.x_data = torch.from_numpy(xy[:, 0:-1])
-        self.y_data = torch.from_numpy(xy[:, [-1]])
+        self.x_data = from_numpy(xy[:, 0:-1])
+        self.y_data = from_numpy(xy[:, [-1]])
 
     def __getitem__(self, index):
         return self.x_data[index], self.y_data[index]
@@ -32,18 +30,18 @@ train_loader = DataLoader(dataset=dataset,
                           num_workers=2)
 
 
-class Model(torch.nn.Module):
+class Model(nn.Module):
 
     def __init__(self):
         """
         In the constructor we instantiate two nn.Linear module
         """
         super(Model, self).__init__()
-        self.l1 = torch.nn.Linear(8, 6)
-        self.l2 = torch.nn.Linear(6, 4)
-        self.l3 = torch.nn.Linear(4, 1)
+        self.l1 = nn.Linear(8, 6)
+        self.l2 = nn.Linear(6, 4)
+        self.l3 = nn.Linear(4, 1)
 
-        self.sigmoid = torch.nn.Sigmoid()
+        self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
         """
@@ -56,15 +54,15 @@ class Model(torch.nn.Module):
         y_pred = self.sigmoid(self.l3(out2))
         return y_pred
 
+
 # our model
 model = Model()
-
 
 # Construct our loss function and an Optimizer. The call to model.parameters()
 # in the SGD constructor will contain the learnable parameters of the two
 # nn.Linear modules which are members of the model.
-criterion = torch.nn.BCELoss(size_average=True)
-optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
+criterion = nn.BCELoss(reduction='sum')
+optimizer = optim.SGD(model.parameters(), lr=0.1)
 
 # Training loop
 for epoch in range(2):
@@ -72,15 +70,12 @@ for epoch in range(2):
         # get the inputs
         inputs, labels = data
 
-        # wrap them in Variable
-        inputs, labels = Variable(inputs), Variable(labels)
-
         # Forward pass: Compute predicted y by passing x to the model
         y_pred = model(inputs)
 
         # Compute and print loss
         loss = criterion(y_pred, labels)
-        print(epoch, i, loss.data[0])
+        print(f'Epoch {epoch + 1} | Batch: {i+1} | Loss: {loss.item():.4f}')
 
         # Zero gradients, perform a backward pass, and update the weights.
         optimizer.zero_grad()
